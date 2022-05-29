55cda5a4e25ec04ab86402fe361088e6728dc2b1
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2022-05-29 20:28:07 +0200

    nav js skonczony

--- app/nav.js
+++ app/nav.js
@@ -1,4 +1,7 @@
 const buttons = document.querySelectorAll('.nav__button');
+const first_page = document.querySelector(".first_page");
+const second_page = document.querySelector(".second_page");
+const third_page = document.querySelector(".third__page");
 
 buttons.forEach((button)=>{
     button.addEventListener("click", ()=>{
@@ -8,6 +11,24 @@ buttons.forEach((button)=>{
                     item.classList.remove("btn_active");
                 }
             });
+
+            if(button.classList.contains("first")){
+                first_page.classList.remove("disabled");
+                second_page.classList.contains("disabled") ? true : second_page.classList.add("disabled");
+                third_page.classList.contains("disabled") ? true : third_page.classList.add("disabled");
+            }
+
+            if(button.classList.contains("second")){
+                second_page.classList.remove("disabled");
+                first_page.classList.contains("disabled") ? true : first_page.classList.add("disabled");
+                third_page.classList.contains("disabled") ? true : third_page.classList.add("disabled");
+            }
+
+            if(button.classList.contains("third")){
+                third_page.classList.remove("disabled");
+                first_page.classList.contains("disabled") ? true : first_page.classList.add("disabled");
+                second_page.classList.contains("disabled") ? true : second_page.classList.add("disabled");
+            }
 
             button.classList.add("btn_active");
         }
