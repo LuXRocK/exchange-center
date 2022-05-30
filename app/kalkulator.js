7739794e7c7bfce6c8e32663fd7d75fbdbba0b42
Bartłomiej Papis <72127774+papiS23@users.noreply.github.com>
2022-05-30 12:08:01 +0200

    ready currency calculator

--- app/kalkulator.js
+++ app/kalkulator.js
@@ -1,27 +1,33 @@
-const amount = document.getElementById("amount")
-const from = document.getElementById("from")
-const to = document.getElementById("to")
-const convert_button = document.getElementById("button_convert")
-const api_url = '/app/json/data.json';
+const amount = document.getElementById("amount");
+const from = document.getElementById("from");
+const to = document.getElementById("to");
+const convert_button = document.getElementById("button_convert");
+const resultText = document.querySelector(".result__text");
+const api_url = "/app/json/data.json";
 
-// convert_button.addEventListener('click', function(){
-    
-// })
-// let dane = fetch("/app/json/data.json").then(response => {
-    
-//     return response.json();
-// }).then(response => {
-//     console.log(response)
-// });
+async function getJson(api_url) {
+  const request = new Request(api_url);
 
-// console.log(dane)
+  const response = await fetch(request);
+  let readyJson = await response.text();
+  readyJson = await JSON.parse(readyJson);
 
-
-
-async function getRates(){
-    const response = await fetch(api_url);
-    const data = await response.json();
-    
-
+  convert_button.addEventListener("click", () => {
+    let result = 0;
+    if (to.value == "PLN" && from.value == "PLN") {
+      result = amount.value;
+    } else if (to.value == "PLN") {
+      result = (amount.value / readyJson.rates[from.value]).toFixed(2);
+    } else if (from.value == "PLN") {
+      result = (amount.value * readyJson.rates[to.value]).toFixed(2);
+    } else {
+      result = (
+        (amount.value / readyJson.rates[from.value]) *
+        readyJson.rates[to.value]
+      ).toFixed(2);
+    }
+    resultText.innerText = `${amount.value} ${from.value} is ${result} ${to.value}`;
+    resultText.classList.remove("result__text--disabled");
+  });
 }
-
+getJson(api_url);
--- app/main_slider.js
+++ app/main_slider.js
@@ -1,59 +1,58 @@
 const next = document.querySelector("#next_btn");
 const prev = document.querySelector("#prev_btn");
 const articles = document.querySelectorAll(".article");
-let articles_list = []
+let articles_list = [];
 
-articles.forEach((article)=>{
-    articles_list.push(article);
-})
+articles.forEach((article) => {
+  articles_list.push(article);
+});
 
 let counter = 0;
 
 const swap = [
-    { transform: 'translatex(-100%) scale(0)' },
-    { transform: 'translatex(0) scale(1)' }
-  ];
-  
-const timing = {
-    duration: 2000,
-    iterations: 1,
-  }
-  
+  { transform: "translatex(-100%) scale(0)" },
+  { transform: "translatex(0) scale(1)" },
+];
 
-next.addEventListener("click", function(){
-    if (counter == 2){
-        counter = 0;
-        articles_list[2].classList.toggle("article--visible");
-        articles_list[counter].classList.toggle("article--visible");
-        console.log(counter)
-    }else if(counter > 0){
-        counter += 1;
-        console.log(counter)
-        articles_list[counter-1].classList.toggle("article--visible");
-        articles_list[counter].classList.toggle("article--visible");
-    }else if(counter == 0){
-        counter += 1;
-        articles_list[0].classList.toggle("article--visible");
-        articles_list[counter].classList.toggle("article--visible");
-        console.log(counter)
-    }
+const timing = {
+  duration: 2000,
+  iterations: 1,
+};
+
+next.addEventListener("click", function () {
+  if (counter == 2) {
+    counter = 0;
+    articles_list[2].classList.toggle("article--visible");
+    articles_list[counter].classList.toggle("article--visible");
+    console.log(counter);
+  } else if (counter > 0) {
+    counter += 1;
+    console.log(counter);
+    articles_list[counter - 1].classList.toggle("article--visible");
+    articles_list[counter].classList.toggle("article--visible");
+  } else if (counter == 0) {
+    counter += 1;
+    articles_list[0].classList.toggle("article--visible");
+    articles_list[counter].classList.toggle("article--visible");
+    console.log(counter);
+  }
 });
 
-prev.addEventListener("click", function(){
-    if (counter == 0){
-        counter = 2;
-        articles_list[0].classList.toggle("article--visible");
-        articles_list[counter].classList.toggle("article--visible");
-        console.log(counter)
-    }else if(counter < 2){
-        counter -= 1;
-        console.log(counter)
-        articles_list[counter+1].classList.toggle("article--visible");
-        articles_list[counter].classList.toggle("article--visible");
-    }else if(counter == 2){
-        counter -= 1;
-        articles_list[2].classList.toggle("article--visible");
-        articles_list[counter].classList.toggle("article--visible");
-        console.log(counter)
-    }
-})
+prev.addEventListener("click", function () {
+  if (counter == 0) {
+    counter = 2;
+    articles_list[0].classList.toggle("article--visible");
+    articles_list[counter].classList.toggle("article--visible");
+    console.log(counter);
+  } else if (counter < 2) {
+    counter -= 1;
+    console.log(counter);
+    articles_list[counter + 1].classList.toggle("article--visible");
+    articles_list[counter].classList.toggle("article--visible");
+  } else if (counter == 2) {
+    counter -= 1;
+    articles_list[2].classList.toggle("article--visible");
+    articles_list[counter].classList.toggle("article--visible");
+    console.log(counter);
+  }
+});
